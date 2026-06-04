b5788416169d414fdb9bfab413612568aae6912e
ntheidanphill <ntheidanphill@gmail.com>
2026-06-04 11:53:28 +0300

    lab 2.7 - js101

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let age = 20;
+var course = "BBIT";
+const university = "Strathmore";
+
+//let age = 40; 
+//var course = "ICS";
+//const university = "Riara";
+
+if (true) {
+  let time = "11:30";
+  var day = "4th";
+  const year = 2026;
+}
+ console.log (year);
+
+
+
+
+
